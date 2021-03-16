4b234c51696eadcf546e6f2721f8177d327f0b4f
Luu <lucas.giancola@live.com>
2021-03-16 11:13:52 -0400

    Bumped version 21.3 -> 23.2

--- plan.sh
+++ plan.sh
@@ -1,14 +1,14 @@
 pkg_name=erlang
 pkg_origin=core
-pkg_version=21.3
+pkg_version=23.2
 pkg_description="A programming language for massively scalable soft real-time systems."
 pkg_upstream_url="http://www.erlang.org/"
 pkg_dirname="otp_src_${pkg_version}"
 pkg_license=('Apache-2.0')
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
-pkg_source="http://www.erlang.org/download/otp_src_${pkg_version}.tar.gz"
+pkg_source="http://erlang.org/download/otp_src_${pkg_version}.tar.gz"
 pkg_filename="otp_src_${pkg_version}.tar.gz"
-pkg_shasum=69a743c4f23b2243e06170b1937558122142e47c8ebe652be143199bfafad6e4
+pkg_shasum=40e03428826c92e409e4f1510f9c0539eafb3ac49d6c2f607f4fa274d11a8928
 pkg_build_deps=(
   core/coreutils
   core/gcc
